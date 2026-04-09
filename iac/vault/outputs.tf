a9634384b9b213966ccc07706299465b01ffc026
Andre Faria <andre.faria.24@gmail.com>
2026-04-09 14:28:51 -0400

    removed vault role creation logic

--- iac/vault/outputs.tf
+++ iac/vault/outputs.tf
@@ -2,3 +2,7 @@ output "roles_anywhere_issuer_certificate" {
   value     = vault_pki_secret_backend_intermediate_set_signed.roles_anywhere.certificate
   sensitive = true
 }
+
+output "vault_pki_backend" {
+  value = vault_mount.pki_intermediate.path
+}
--- iac/vault/variables.tf
+++ iac/vault/variables.tf
@@ -11,20 +11,3 @@ variable "domain_name" {
   type    = string
   default = "example.com"
 }
-
-variable "spiffe_trust_domain" {
-  type    = string
-  default = "example"
-}
-
-variable "pki_roles" {
-  description = "PKI roles to create in Vault for AWS Roles Anywhere certificate issuance."
-
-  type = map(object({
-    team         = string
-    environment  = string
-    organization = optional(list(string), ["example"])
-    ttl          = optional(string, "1200")
-    max_ttl      = optional(string, "1200")
-  }))
-}
