67b3316c33faf087c5dcaa40b9ce8ed7399abab6
Andre Faria <andre.faria.24@gmail.com>
2026-04-09 15:30:00 -0400

    removed hardcoded values

--- scripts/ListS3Buckets.ps1
+++ scripts/ListS3Buckets.ps1
@@ -1,11 +1,32 @@
+param(
+  [Parameter(Mandatory = $true)]
+  [string]$TrustAnchorArn,
+
+  [Parameter(Mandatory = $true)]
+  [string]$ProfileArn,
+
+  [Parameter(Mandatory = $true)]
+  [string]$RoleArn,
+
+  [string]$CertificatePath = ".\\certs\\cert.pem",
+
+  [string]$PrivateKeyPath = ".\\certs\\key.pem",
+
+  [string]$Region = "us-east-2",
+
+  [string]$Endpoint = "https://rolesanywhere.us-east-2.amazonaws.com"
+)
+
+$ErrorActionPreference = "Stop"
+
 $creds = aws_signing_helper credential-process `
-  --certificate .\certs\cert.pem `
-  --private-key .\certs\key.pem `
-  --trust-anchor-arn arn:aws:rolesanywhere:us-east-2:337501927775:trust-anchor/b6ab79ec-d9e7-4028-9dd1-9fa98f1dbadc `
-  --profile-arn arn:aws:rolesanywhere:us-east-2:337501927775:profile/00c55659-c86f-4321-a258-f5b126312fde `
-  --role-arn arn:aws:iam::337501927775:role/HCPVaultAssumeRoleForTeam1App1 `
-  --region us-east-2 `
-  --endpoint https://rolesanywhere.us-east-2.amazonaws.com | ConvertFrom-Json
+  --certificate $CertificatePath `
+  --private-key $PrivateKeyPath `
+  --trust-anchor-arn $TrustAnchorArn `
+  --profile-arn $ProfileArn `
+  --role-arn $RoleArn `
+  --region $Region `
+  --endpoint $Endpoint | ConvertFrom-Json
 
 $env:AWS_ACCESS_KEY_ID     = $creds.AccessKeyId
 $env:AWS_SECRET_ACCESS_KEY = $creds.SecretAccessKey
