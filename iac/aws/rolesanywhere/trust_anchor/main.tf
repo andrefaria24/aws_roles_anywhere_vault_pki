7e0718bd883c4a4798f1b02b199b561226ea393f
Andre Faria <andre.faria.24@gmail.com>
2026-04-15 09:47:24 -0400

    removed commented out lines

--- iac/aws/rolesanywhere/trust_anchor/main.tf
+++ iac/aws/rolesanywhere/trust_anchor/main.tf
@@ -1,5 +1,4 @@
 data "tls_certificate" "cert" {
-  #content = vault_pki_secret_backend_intermediate_set_signed.roles_anywhere.certificate
   content = file("${path.module}/certs/vault_intermediate_cert.pem")
 }
 
@@ -8,16 +7,8 @@ resource "aws_rolesanywhere_trust_anchor" "hcp_vault" {
   enabled = true
   source {
     source_data {
-      #x509_certificate_data = "${trimspace(vault_pki_secret_backend_intermediate_set_signed.roles_anywhere.certificate)}\n"
       x509_certificate_data = "${trimspace(file("${path.module}/certs/vault_intermediate_cert.pem"))}\n"
     }
     source_type = "CERTIFICATE_BUNDLE"
   }
-  # tags = {
-  #   serial_number       = vault_pki_secret_backend_root_sign_intermediate.roles_anywhere.serial_number
-  #   cert_expiration_iso = data.tls_certificate.cert.certificates[0].not_after
-  # }
-
-  # Wait for the signed intermediate certificate to be ready
-  #depends_on = [vault_pki_secret_backend_intermediate_set_signed.roles_anywhere]
 }
